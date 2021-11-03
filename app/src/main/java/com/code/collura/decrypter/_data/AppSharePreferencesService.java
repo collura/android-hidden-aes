6dd0f9bf0a0b8603e647b7ef158d23436cb91c59
collura <collura.developer@gmail.com>
2021-11-02 21:43:35 -0300

    code improve.

--- app/src/main/java/com/code/collura/decrypter/_data/AppSharePreferencesService.java
+++ app/src/main/java/com/code/collura/decrypter/_data/AppSharePreferencesService.java
@@ -12,29 +12,54 @@ import java.util.List;
 import java.util.Objects;
 
 public class AppSharePreferencesService implements SharedPreferencesService{
-    private final StringBuilder keyTokey = new StringBuilder();
+    private final StringBuilder horse = new StringBuilder();
 
     public AppSharePreferencesService(){
-        final List<String> caracters = new ArrayList<>(Arrays.asList(
-                "$", "U", "c", "3", "S", "s", "2", "0", "0", "1", "#", "@", "D", "3", "v", "=", "=", "+", "%"
+        final List<String> caracters = new ArrayList<>(Arrays.asList( // >= 20
+                "$", "U", "c", "3", "S", "s",
+                "2", "0", "-", "1", "#", "@",
+                "D", "3", "v", "*", "=", "+",
+                "%",")","!"
         ));
-        for(int i = 0; i < caracters.size(); i++)
-            keyTokey.append(i);
+
+        // =========================================================
+        // GENERATING THE KEY TO ENCRYPT THE USER KEY AND STORE
+        // =========================================================
+        horse.append(caracters.get(2));
+        horse.append(caracters.get(1));
+        horse.append(caracters.get(4));
+        horse.append(caracters.get(3));
+        horse.append(caracters.get(6));
+        horse.append(caracters.get(5));
+        horse.append(caracters.get(8));
+        horse.append(caracters.get(7));
+        horse.append(caracters.get(10));
+        horse.append(caracters.get(9));
+        horse.append(caracters.get(12));
+        horse.append(caracters.get(11));
+        horse.append(caracters.get(13));
+        horse.append(caracters.get(14));
+        horse.append(caracters.get(16));
+        horse.append(caracters.get(15));
+        horse.append(caracters.get(18));
+        horse.append(caracters.get(17));
+        horse.append(caracters.get(20));
+        horse.append(caracters.get(19));
     }
 
     @Override
-    public void setKey( String key ) {
+    public void setKey( String stonks ) {
         SharedPreferences.Editor editor = MyApplication.preferences.edit();
         String encryptKey = Objects.requireNonNull(
-                CriptografiaService.getDefault(new RequestCriptografia(keyTokey.toString(),
-                        key))).encrypt().getTexto();
-        editor.putString("key",encryptKey );
+                CriptografiaService.getDefault(new RequestCriptografia(horse.toString(),
+                        stonks))).encrypt().getTexto();
+        editor.putString("sarcofago",encryptKey );
         editor.apply();
     }
 
     @Override
     public String getKey() {
-        String key = MyApplication.preferences.getString("key", "");
-        return Objects.requireNonNull(CriptografiaService.getDefault(new RequestCriptografia(keyTokey.toString(), key))).decrypt().getTexto();
+        String key = MyApplication.preferences.getString("sarcofago", "");
+        return Objects.requireNonNull(CriptografiaService.getDefault(new RequestCriptografia(horse.toString(), key))).decrypt().getTexto();
     }
 }
